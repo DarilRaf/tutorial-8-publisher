6b6e8b46db6a7c82453608819456be2fe3471411
DarilRaf <darilmrafansyah@gmail.com>
2024-04-24 13:24:54 +0700

    Initial publisher code.

---
+++ target/debug/deps/publisher.d
@@ -0,0 +1,5 @@
+C:\Users\Daril\Desktop\tutor8\tutor8-publisher\target\debug\deps\publisher.exe: src\main.rs
+
+C:\Users\Daril\Desktop\tutor8\tutor8-publisher\target\debug\deps\publisher.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/publisher.d
@@ -0,0 +1,1 @@
+C:\Users\Daril\Desktop\tutor8\tutor8-publisher\target\debug\publisher.exe: C:\Users\Daril\Desktop\tutor8\tutor8-publisher\src\main.rs
